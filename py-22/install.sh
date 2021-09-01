d5348e6ff709e62d92a5085af4151f48a32a8d29
Robert Craigie <robertcraigie30@gmail.com>
2021-09-01 15:15:39 +0100

    [py-22]: switch to using git patches instead of specific branch

--- py-22/install.sh
+++ py-22/install.sh
@@ -8,8 +8,13 @@ set +x
 source .venv/bin/activate
 set -x
 
-git clone --branch 'reproductions/22' https://github.com/RobertCraigie/prisma-client-py
+git clone --branch 'main' https://github.com/RobertCraigie/prisma-client-py
 
 cd prisma-client-py
+
+git reset --hard ebd80aa1a7447f4dade2501914636b0f7f896ed6
+
+git am ../patches/0001-Delegate-binary-downloading-to-prisma.patch
+
 pip install -U -e .[aiohttp]
 cd ..
